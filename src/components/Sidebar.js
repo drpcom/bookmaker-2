75196629fc7dffd2324a4b4ec74209ecc5395195
Jamie <drpcom@yahoo.com>
2022-01-29 19:03:26 -0600

    Fixed sidebar bg color error.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Sidebar = () => {
     return (
-        <div className="md:w-2/5 bg-violet-300 h-full hidden md:flex md:flex-col md:h-auto spec:h-full">
+        <div className="md:w-2/5 bg-violet-300 h-fit min-h-screen hidden md:flex md:flex-col">
           <div className="flex flex-col items-center h-12 justify-center m-4">
               <input className="w-[97%] h-11/12 text-2xl indent-2 p-2" type="text" placeholder="Search" />
           </div>
           <div className='flex flex-row justify-center flex-wrap ml-4 xl:justify-start'>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
+            <button className='bg-emerald-50 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2'>
                 <div className='text-3xl'>
                     <p className='text-center mb-2'>title</p> 
                 </div>
@@ -15,7 +15,7 @@ const Sidebar = () => {
                    <p className='text-center'>body</p> 
                 </div>    
             </button>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
+            <button className='bg-emerald-50 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2'>
                 <div className='text-3xl'>
                     <p className='text-center mb-2'>title</p> 
                 </div>
@@ -23,7 +23,7 @@ const Sidebar = () => {
                    <p className='text-center'>body</p> 
                 </div>    
             </button>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
+            <button className='bg-emerald-50 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2'>
                 <div className='text-3xl'>
                     <p className='text-center mb-2'>title</p> 
                 </div>
@@ -31,7 +31,7 @@ const Sidebar = () => {
                    <p className='text-center'>body</p> 
                 </div>    
             </button>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
+            <button className='bg-emerald-50 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2'>
                 <div className='text-3xl'>
                     <p className='text-center mb-2'>title</p> 
                 </div>
@@ -39,7 +39,7 @@ const Sidebar = () => {
                    <p className='text-center'>body</p> 
                 </div>    
             </button>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
+            <button className='bg-emerald-50 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2'>
                 <div className='text-3xl'>
                     <p className='text-center mb-2'>title</p> 
                 </div>
@@ -47,7 +47,7 @@ const Sidebar = () => {
                    <p className='text-center'>body</p> 
                 </div>    
             </button>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
+            <button className='bg-emerald-50 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2'>
                 <div className='text-3xl'>
                     <p className='text-center mb-2'>title</p> 
                 </div>
@@ -55,7 +55,7 @@ const Sidebar = () => {
                    <p className='text-center'>body</p> 
                 </div>    
             </button>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
+            <button className='bg-emerald-50 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2'>
                 <div className='text-3xl'>
                     <p className='text-center mb-2'>title</p> 
                 </div>
@@ -63,31 +63,6 @@ const Sidebar = () => {
                    <p className='text-center'>body</p> 
                 </div>    
             </button>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
-                <div className='text-3xl'>
-                    <p className='text-center mb-2'>title</p> 
-                </div>
-                <div>
-                   <p className='text-center'>body</p> 
-                </div>    
-            </button>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
-                <div className='text-3xl'>
-                    <p className='text-center mb-2'>title</p> 
-                </div>
-                <div>
-                   <p className='text-center'>body</p> 
-                </div>    
-            </button>
-            <button className='bg-emerald-50 rounded w-32 h-36 spec:w-44 spec:h-48 shadow-md shadow-slate-700/50 m-2'>
-                <div className='text-3xl'>
-                    <p className='text-center mb-2'>title</p> 
-                </div>
-                <div>
-                   <p className='text-center'>body</p> 
-                </div>    
-            </button>
-            
           </div>
         </div>
       )
