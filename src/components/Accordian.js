728b972f6b64d6ad811f5ecbf22109b8b36c8be8
Jamie <drpcom@yahoo.com>
2022-01-31 15:13:12 -0600

    Fully implemented accordian.

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -3,52 +3,46 @@ import 'tw-elements';
 
 const Accordian = () => {
   return (
-    <div className="accordion" id="accordionExample5">
-    <div className="accordion-item bg-white border border-gray-200">
-      <h2 className="accordion-header mb-0" id="headingOne5">
-        <button
-          className="
-            accordion-button
-            relative
-            flex
-            items-center
-            w-full
-            py-4
-            px-5
-            text-base text-gray-800 text-left
-            dark:text-slate-200
-            bg-white
-            dark:bg-zinc-600
-            border-0
-            rounded-none
-            transition
-            focus:outline-none
-          "
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapseOne5"
-          aria-expanded="true"
-          aria-controls="collapseOne5"
-        >
-          Accordion Item #1
-        </button>
-      </h2>
-      <div
-        id="collapseOne5"
-        className="accordion-collapse collapse show"
-        aria-labelledby="headingOne5"
-      >
-        <div className="accordion-body py-4 px-5">
-          <strong>This is the first item's accordion body.</strong> It is shown by default,
-          until the collapse plugin adds the appropriate classes that we use to style each
-          element. These classes control the overall appearance, as well as the showing and
-          hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-          our default variables. It's also worth noting that just about any HTML can go within
-          the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
+      <>
+<div className="collapse" id="collapseExample">
+  <div className="block md:hidden shadow-lg bg-violet-300 dark:bg-[#2e2e37] dark:text-slate-200 overflow-x-scroll">
+  <div className='flex flex-row xl:justify-start'>
+            <button className='bg-emerald-50 dark:bg-zinc-700 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2 flex flex-col p-2 lg:p-3 hover:animate-pulse'>
+                <div className='text-xl lg:text-2xl indent-2'>
+                    <p className='mb-2 line-clamp-1 h-8 text-left dark:text-sky-300'>oh canada you are my favorite place</p> 
+                </div>
+                <div>
+                   <p className='line-clamp-5 text-xs flex flex-row text-left lg:line-clamp-7 dark:text-slate-200'>I really didn’t expect The Brothers Karamazov to have much of an effect on me after I finished it. It didn’t knock me off my feet. But I’ve caught myself thinking about the characters a lot. The characters really do stay with you. Each one is so well-drawn. Mitya, Ivan, Alyosha, #Grushenka, Katerina Ivanovna, Smerdyakov, Lise, Madame Khokhlakov. Even characters like Snegiryov and Kolya Krasotkin. There’s just no flatness to any of them. Dostovesky didn’t know how to write a flat character.</p> 
+                </div>    
+            </button>
+            <button className='bg-emerald-50 dark:bg-zinc-700 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2 flex flex-col p-2 lg:p-3 hover:animate-pulse'>
+                <div className='text-xl lg:text-2xl indent-2'>
+                    <p className='mb-2 line-clamp-1 h-8 text-left dark:text-sky-300'>sprinter!</p> 
+                </div>
+                <div>
+                   <p className='line-clamp-5 text-xs flex flex-row text-left lg:line-clamp-7 dark:text-slate-200'>Recently, there has been a renewed interest in emo, especially midwest emo. These bands are heavily influenced by Cap'n Jazz and often have a strong bent towards math rock and post-rock in their song structure and guitar work. A few examples of these twinkledaddy bands are Street Smart Cyclist, Snowing, Algernon Cadwallader, The World Is A Beautiful Place & I Am No Longer Afraid to Die, and Glocca Morra. As this scene grew and grew in popularity, #emorevival became a near-mainstream style of indie rock which took from various facets of Midwest, screamo, and emo pop, as played by bands such as the Hotelier, Free Throw, and late-period You Blew It!</p> 
+                </div>    
+            </button>
+            <button className='bg-emerald-50 dark:bg-zinc-700 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2 flex flex-col p-2 lg:p-3 hover:animate-pulse'>
+                <div className='text-xl lg:text-2xl indent-2'>
+                    <p className='mb-2 line-clamp-1 h-8 text-left dark:text-sky-300'>oh canada you are my favorite place</p> 
+                </div>
+                <div>
+                   <p className='line-clamp-5 text-xs flex flex-row text-left lg:line-clamp-7 dark:text-slate-200'>I really didn’t expect The Brothers Karamazov to have much of an effect on me after I finished it. It didn’t knock me off my feet. But I’ve caught myself thinking about the characters a lot. The characters really do stay with you. Each one is so well-drawn. Mitya, Ivan, Alyosha, #Grushenka, Katerina Ivanovna, Smerdyakov, Lise, Madame Khokhlakov. Even characters like Snegiryov and Kolya Krasotkin. There’s just no flatness to any of them. Dostovesky didn’t know how to write a flat character.</p> 
+                </div>    
+            </button>          
+            <button className='bg-emerald-50 dark:bg-zinc-700 rounded w-32 h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2 flex flex-col p-2 lg:p-3 hover:animate-pulse'>
+                <div className='text-xl lg:text-2xl indent-2'>
+                    <p className='mb-2 line-clamp-1 h-8 text-left dark:text-sky-300'>oh canada you are my favorite place</p> 
+                </div>
+                <div>
+                   <p className='line-clamp-5 text-xs flex flex-row text-left lg:line-clamp-7 dark:text-slate-200'>I really didn’t expect The Brothers Karamazov to have much of an effect on me after I finished it. It didn’t knock me off my feet. But I’ve caught myself thinking about the characters a lot. The characters really do stay with you. Each one is so well-drawn. Mitya, Ivan, Alyosha, #Grushenka, Katerina Ivanovna, Smerdyakov, Lise, Madame Khokhlakov. Even characters like Snegiryov and Kolya Krasotkin. There’s just no flatness to any of them. Dostovesky didn’t know how to write a flat character.</p> 
+                </div>    
+            </button>          
+          </div>
   </div>
+</div>
+      </>
   )
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -76,9 +76,9 @@ const Header = () => {
             </div>
 
           </div>
-          <div className='flex flex-row md:hidden'>
+          <button className='flex flex-row md:hidden' data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
             <svg className="w-8 h-8 text-slate-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
-          </div>
+          </button>
         </div>
       </div>
       </div>
