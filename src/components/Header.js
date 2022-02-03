7ebce8088555648d166f2806ca9e4be7f8c927be
Jamie <drpcom@yahoo.com>
2022-02-03 17:50:32 -0600

    Implemented zoom buttons logic.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,13 @@ import React from 'react';
 import DarkMode from './darkMode';
 import { useCollapse } from '../context/collapseContext';
 import { useCollapseSidebar } from '../context/collapseSidebarContext';
+import { handleZoomIn } from './zoomBtns';
+import { handleZoomOut } from './zoomBtns';
 
 const Header = () => {
   const { clicked, handleClick } = useCollapse();
   const { open, handleToggle } = useCollapseSidebar();
+
   return (
       <div>
         <div className="h-1/3 bg-slate-400 dark:bg-zinc-600">
@@ -46,7 +49,7 @@ const Header = () => {
               <div className='text-slate-100 text-2xl mr-4 ml-4 hidden sm:flex'>||</div>
             <div className='flex text-lg z-10 justify-around w-40'>
                 <div>
-                  <button data-tooltip-target="zoom-in-tooltip" type="button">
+                  <button onClick={() => handleZoomIn()} data-tooltip-target="zoom-in-tooltip" type="button">
                       <svg className="w-7 h-7 text-slate-600 dark:text-sky-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" /></svg>
                   </button>
                   <div id="zoom-in-tooltip" role="tooltip" className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
@@ -55,7 +58,7 @@ const Header = () => {
                 </div>
                 </div>
                 <div>
-                  <button data-tooltip-target="zoom-out-tooltip" type="button">
+                  <button onClick={() => handleZoomOut()} data-tooltip-target="zoom-out-tooltip" type="button">
                       <svg className="w-7 h-7 text-slate-600 dark:text-sky-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM13 10H7" /></svg>
                   </button>
                   <div id="zoom-out-tooltip" role="tooltip" className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
