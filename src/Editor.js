2b555d458e1b57274b75cc02d1aeedfaae0d026d
Jamie <drpcom@yahoo.com>
2022-02-04 21:17:03 -0600

    Erroring out because of selectedNote.

--- src/Editor.js
+++ src/Editor.js
@@ -1,25 +1,60 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactQuill from "react-quill";
 import EditorToolbar, { modules, formats } from "./EditorToolbar";
 import "react-quill/dist/quill.snow.css";
+import { useNotes } from "./context/notesContext";
 
 export const Editor = () => {
-    const [state, setState] = React.useState({ value: null });
-    const handleChange = value => {
-      setState({ value });
+
+    const { selectedNote, setSelectedNote, selectedNoteIndex, setSelectedNoteIndex, notes, setNotes, NoteUpdate } = useNotes();
+
+    const [text, setText] = useState('');
+    const [title, setTitle] = useState('');
+    const [id, setId] = useState('');
+
+    useEffect(() => {
+        setText(selectedNote.body);
+        setTitle(selectedNote.title);
+        setId(selectedNote.id);   
+        if(selectedNote.id !== id) {
+            setText(selectedNote.body);
+            setTitle(selectedNote.title);
+            setId(selectedNote.id);
+        }
+    },[id, selectedNote.body, selectedNote.title, selectedNote.id]);
+
+    const UpdateBody = async (val) => {
+        try {
+            await setText(val);
+            Update();
+            
+        } catch (error) {
+            console.log("nah.")
+        }
     };
+    const UpdateTitle = async (txt) => {
+        await setTitle(txt);
+        Update();
+    };
+    const Update = () => {
+        //debounce used to go here
+        NoteUpdate(id, {
+            title: title,
+            body: text
+        })
+    };
+
+
     return (
-      <div className="text-editor">
-        <EditorToolbar />
-        <ReactQuill
-          theme="snow"
-          value={state.value}
-          onChange={handleChange}
-          placeholder={"Write something awesome..."}
-          modules={modules}
-          formats={formats}
-        />
-      </div>
+            <div className="text-editor">
+                <EditorToolbar />
+                <ReactQuill
+                theme="snow"
+                placeholder={"Write something awesome..."}
+                modules={modules}
+                formats={formats}
+                />
+            </div>
     );
   };
   
