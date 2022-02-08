635f94cabf42fa1d62fc73436da8bc979587c629
Jamie <drpcom@yahoo.com>
2022-02-07 23:23:05 -0600

    UI now looks right even when no document is selected.

--- src/components/Center.js
+++ src/components/Center.js
@@ -11,8 +11,8 @@ const Center = () => {
 
   return (
     <>
-    <div className={open ? "w-full md:w-3/5 h-full bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center" : "w-full md:w-[60%] h-full bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center"}>
-      <div className='h-full w-[97%] mt-3 dark:bg-zinc-700 text-zinc-700 dark:text-slate-200'>
+    <div className={open ? "sidebar-shown min-h-screen md:h-auto w-full md:w-3/5 bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center" : "sidebar-hidden w-full md:w-[60%] bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center"}>
+      <div className={selectedNote ? 'w-[97%] mt-3 bg-[#F3F4F6] dark:bg-zinc-700 text-zinc-700 dark:text-slate-200 rounded' : 'no-selected-item min-h-screen w-[97%] mt-3 bg-[#F3F4F6] dark:bg-zinc-700 text-zinc-700 dark:text-slate-200 rounded'}>
         <div className='bg-[#F3F4F6] dark:bg-[#3F3F46] rounded mb-16'>
           <input type="text" placeholder="Titulo" className="w-[80%] m-4 border-0 focus:ring-0 text-3xl bg-gray-100 dark:bg-zinc-700 text-zinc-700 dark:text-slate-200" />
           {selectedNote ? <Editor /> : null}
