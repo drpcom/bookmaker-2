4f6dba41b2aae51be72af2750082e2ee4c2f555d
Jamie <drpcom@yahoo.com>
2022-02-09 15:05:36 -0600

    Fixed editor height on the condition that there are many documents in the sidebar.

--- src/components/Center.js
+++ src/components/Center.js
@@ -11,7 +11,7 @@ const Center = () => {
 
   return (
     <>
-    <div className={open ? "sidebar-shown min-h-screen md:h-auto w-full md:w-3/5 bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center" : "sidebar-hidden w-full md:w-[60%] bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center"}>
+    <div className={open ? "sidebar-shown min-h-screen h-fit w-full md:w-3/5 bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center" : "sidebar-hidden w-full md:w-[60%] bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center"}>
       <div className={selectedNote ? 'w-[97%] mt-3 bg-[#F3F4F6] dark:bg-zinc-700 text-zinc-700 dark:text-slate-200 rounded' : 'no-selected-item min-h-screen w-[97%] mt-3 bg-[#F3F4F6] dark:bg-zinc-700 text-zinc-700 dark:text-slate-200 rounded'}>
         <div className='bg-[#F3F4F6] dark:bg-[#3F3F46] rounded mb-16'>
           {selectedNote ? <Editor /> : null}
