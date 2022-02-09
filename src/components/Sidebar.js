285399131c69f59648591fa9c7d7248dc86da514
Jamie <drpcom@yahoo.com>
2022-02-09 14:44:49 -0600

    Added margin bottom to sidebar so bottom document is visible when toolbar is showing.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,7 +10,7 @@ const Sidebar = () => {
 
     return (
         <>       
-        <div className={open ? "md:w-2/5 bg-violet-300 dark:bg-[#2e2e37] h-fit min-h-screen hidden md:flex md:flex-col" : "hidden"}>
+        <div className={open ? "md:w-2/5 bg-violet-300 dark:bg-[#2e2e37] h-fit min-h-screen hidden md:flex md:flex-col mb-16" : "hidden"}>
           <div className="flex flex-row items-center h-12 mt-4 mb-4 ml-2 pl-4 pr-4">
               <Create />
               <input type="search" className="form-control ml-6 min-w-0 block h-11/12 text-2xl text-slate-200 dark:text-white bg-white dark:bg-zinc-600 bg-clip-padding border-none m-0 focus:text-gray-700 focus:bg-white focus:border-sky-300 focus:outline-none" placeholder="Search" aria-label="Search"></input>
