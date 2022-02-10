653d7cea75bf6f9ccade56918c60b8dc0c3b4ddc
Jamie <drpcom@yahoo.com>
2022-02-10 00:12:06 -0600

    Changed light mode button colors for better palette match.

--- src/components/NewDoc.js
+++ src/components/NewDoc.js
@@ -12,7 +12,7 @@ export default function Create() {
 
     return (
         <>
-            <button onClick={NewNoteBtnClick} className="flex flex-row text-slate-100 items-center bg-rose-500 dark:bg-sky-400 rounded-sm h-full w-12 justify-center" data-tooltip-target="create-tooltip" type="button">
+            <button onClick={NewNoteBtnClick} className="flex flex-row text-slate-100 items-center bg-[#7a7ab8] dark:bg-sky-400 rounded-sm h-full w-12 justify-center" data-tooltip-target="create-tooltip" type="button">
                 <svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>
             </button>
 
