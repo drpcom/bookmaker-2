6b4af82778e41aed60f7e95b830603f466f6eca5
Jamie <drpcom@yahoo.com>
2022-02-10 20:41:16 -0600

    Editor title changed from input to textarea.

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -7,7 +7,7 @@ import './Editor.css';
 
 export const Editor = () => {
 
-    const { selectedNote, setSelectedNote, NoteUpdate } = useNotes();
+    const { selectedNote, setSelectedNote, setSelectedNoteIndex, NoteUpdate } = useNotes();
 
     const [text, setText] = useState('');
     const [title, setTitle] = useState('');
@@ -47,6 +47,7 @@ export const Editor = () => {
 
     const Unselect = () => {
         setSelectedNote(null);
+        setSelectedNoteIndex(null);
     }
 
 
@@ -56,16 +57,16 @@ export const Editor = () => {
                 <div className="text-editor h-full break-words">
                     <EditorToolbar />
                     <div className="flex flex-row justify-around">
-                        <input 
+                        <textarea 
                         value={title ? title : "(No Subject)"} 
                         onChange={(e) => UpdateTitle(e.target.value)}
                         type="text" 
                         placeholder="Title" 
-                        className="w-[80%] flex flex-row flex-wrap m-4 border-0 focus:ring-0 text-3xl bg-gray-100 dark:bg-zinc-700 text-zinc-700 dark:text-slate-200" 
+                        className="resize-none overflow-y-hidden h-20 w-[80%] flex flex-row flex-wrap m-4 border-0 focus:ring-0 text-3xl bg-gray-100 dark:bg-zinc-700 text-zinc-700 dark:text-slate-200" 
                         />
                         {selectedNote ? 
                         <button onClick={Unselect} className="h-6 self-center mr-4">
-                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+                            <svg className="w-6 h-6 text-[#708090]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
                         </button> 
                         : <div></div>}
                     </div>
