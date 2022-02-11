16545516fa1f5eb3460d9c515aae166ba604c2db
Jamie <drpcom@yahoo.com>
2022-02-10 19:37:47 -0600

    Added 'x' button to close selected doc. Also added content that shows in the editor if there is no selected doc.

--- src/components/Center.js
+++ src/components/Center.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useCollapseSidebar } from '../context/collapseSidebarContext';
 import { useNotes } from '../context/notesContext';
 import Editor from './Editor';
+import Create from './NewDoc';
 
 const Center = () => {
   const { open } = useCollapseSidebar();
@@ -14,7 +15,13 @@ const Center = () => {
     <div className={open ? "sidebar-shown min-h-screen h-fit w-full md:w-3/5 bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center" : "sidebar-hidden w-full md:w-[60%] bg-stone-200 dark:bg-[#3b3b41] flex flex-col items-center"}>
       <div className={selectedNote ? 'w-[97%] mt-3 bg-[#F3F4F6] dark:bg-zinc-700 text-zinc-700 dark:text-slate-200 rounded' : 'no-selected-item min-h-screen w-[97%] mt-3 bg-[#F3F4F6] dark:bg-zinc-700 text-zinc-700 dark:text-slate-200 rounded'}>
         <div className='bg-[#F3F4F6] dark:bg-[#3F3F46] rounded mb-16'>
-          {selectedNote ? <Editor /> : null}
+          {selectedNote ? <Editor /> 
+          : <div className='flex justify-center text-2xl md:text-3xl text-zinc-500 mt-44 ml-12 mr-12 select-none'>
+              <div className='h-10 text-center mr-4'>
+                <Create />
+              </div>           
+              <div>Create a Document. Otherwise, select a document you've already created.</div>
+            </div>}
         </div>
       </div>
     </div>
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -7,13 +7,13 @@ import './Editor.css';
 
 export const Editor = () => {
 
-    const { selectedNote, NoteUpdate } = useNotes();
+    const { selectedNote, setSelectedNote, NoteUpdate } = useNotes();
 
     const [text, setText] = useState('');
     const [title, setTitle] = useState('');
     const [id, setId] = useState('');
 
-    useEffect(() => { //i think this is attempting to fetch the current selected note. it doesn't work afaik.
+    useEffect(() => { 
         setText(selectedNote.body);
         setTitle(selectedNote.title);
         setId(selectedNote.id);   
@@ -45,18 +45,30 @@ export const Editor = () => {
         })
     };
 
+    const Unselect = () => {
+        setSelectedNote(null);
+    }
+
 
     return (
         <div className="bg-gray-100 dark:bg-zinc-700 rounded-lg min-h-screen flex flex-col items-center overflow-auto">
             <div className="bg-gray-100 dark:bg-zinc-700 w-full rounded flex flex-col">
                 <div className="text-editor h-full break-words">
                     <EditorToolbar />
-                    <input 
-                    value={title ? title : "(No Subject)"} 
-                    onChange={(e) => UpdateTitle(e.target.value)}
-                    type="text" 
-                    placeholder="Title" 
-                    className="w-[90%] flex flex-row flex-wrap m-4 border-0 focus:ring-0 text-3xl bg-gray-100 dark:bg-zinc-700 text-zinc-700 dark:text-slate-200" />
+                    <div className="flex flex-row justify-around">
+                        <input 
+                        value={title ? title : "(No Subject)"} 
+                        onChange={(e) => UpdateTitle(e.target.value)}
+                        type="text" 
+                        placeholder="Title" 
+                        className="w-[80%] flex flex-row flex-wrap m-4 border-0 focus:ring-0 text-3xl bg-gray-100 dark:bg-zinc-700 text-zinc-700 dark:text-slate-200" 
+                        />
+                        {selectedNote ? 
+                        <button onClick={Unselect} className="h-6 self-center mr-4">
+                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+                        </button> 
+                        : <div></div>}
+                    </div>
                     <ReactQuill
                     value={text}
                     onChange={UpdateBody}
