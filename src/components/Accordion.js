d31ca4dffbd653af93c4169ce9dd6b2ce2d1b88b
Jamie <drpcom@yahoo.com>
2022-02-12 11:41:06 -0600

    Resized accordion docs to be larger, adding better UX.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -18,7 +18,7 @@ const Accordian = () => {
 <div className="collapse" id="collapseExample">
   <div className="flex flex-col md:hidden min-h-screen shadow-lg bg-violet-300 dark:bg-[#2e2e37] dark:text-slate-200 pt-4">
         <input value={searchQuery} onChange={handleSearch} type="search" className="form-control flex self-center min-w-0 w-[300px] sm:w-[400px] h-11/12 text-2xl text-slate-200 dark:text-white bg-[#f3f4f6] dark:bg-zinc-700 rounded-sm bg-clip-padding border-none focus:text-gray-700 focus:bg-white focus:border-sky-300 focus:outline-none mb-4" placeholder="Search" aria-label="Search"></input>
-        <div className='flex flex-row flex-wrap max-w-screen-sm justify-center xl:justify-start'>
+        <div className='flex flex-row flex-wrap max-w-screen-sm justify-center self-center xl:justify-start'>
         {notes ? 
                 notes.filter(_note => {
                     return _note.title?.toLowerCase().match(searchQuery.toLowerCase()) ||
--- src/components/Document.js
+++ src/components/Document.js
@@ -24,7 +24,7 @@ const Document = ({_note, _index}) => {
     
   return (
       <>
-    <button onClick={() => xSelectNote(_note, _index)} onMouseEnter={() => {setIsHovering(true)}} onMouseLeave={() => {setIsHovering(false)}} className='break-all bg-[#f3f4f6] dark:bg-zinc-700 border-8 border-zinc-200 dark:border-[#3b3b41] rounded w-24 h-28 md:w-32 md:h-36 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2 flex flex-col p-2 lg:p-3'>
+    <button onClick={() => xSelectNote(_note, _index)} onMouseEnter={() => {setIsHovering(true)}} onMouseLeave={() => {setIsHovering(false)}} className='break-all bg-[#f3f4f6] dark:bg-zinc-700 border-8 border-zinc-200 dark:border-[#3b3b41] rounded w-36 h-40 lg:w-44 lg:h-48 shadow-md shadow-slate-700/50 m-2 flex flex-col p-2 lg:p-3'>
         <div className='text-xl lg:text-2xl indent-2 flex flex-row w-full justify-between'>
             <p className='mb-2 line-clamp-1 w-[80%] h-6 md:h-8 text-left text-[#708090] dark:text-sky-300 text-sm md:text-2xl'>{_note.title}</p> 
             {isHovering ? <svg onClick={() => xDeleteNote(_note)} className="w-6 h-6 text-[#708090] hover:text-slate-400 ease-in-out duration-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg> : null}
