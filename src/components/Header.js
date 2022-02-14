68765e750a0826b296cc5ad7052f559103cac0e3
Jamie <drpcom@yahoo.com>
2022-02-14 12:01:42 -0600

    Made download button its own component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ import { useNotes } from '../context/notesContext';
 import { handleZoomIn } from './zoomBtns';
 import { handleZoomOut } from './zoomBtns';
 import Create from './NewDoc';
-import { specific } from 'react-files-hooks';
-
+import DownloadBtn from './DownloadBtn';
+import { useDownloader, useUploader, specific, MIME_TYPES } from 'react-files-hooks';
 
 const Header = () => {
   const { clicked, handleClick } = useCollapse();
@@ -31,54 +31,7 @@ const Header = () => {
   //   onError: error => {}
   // });
 
-  function extractContent(s, space) {
-    var span= document.createElement('span');
-    span.innerHTML= s;
-    if(space) {
-      var children= span.querySelectorAll('*');
-      for(var i = 0 ; i < children.length ; i++) {
-        if(children[i].textContent)
-          children[i].textContent+= ' ';
-        else
-          children[i].innerText+= ' ';
-      }
-    }
-    return [span.textContent || span.innerText].toString().replace(/ +/g,' ');
-  };
-      
-  const txtBody = () => {
-    if (selectedNote) {
-      let bodyTextOnly = extractContent(selectedNote.body, true)
-      return bodyTextOnly;
-    } else {
-      return "For some reason, your download didn't work :("
-    }
-  }
-
-  const txtTitle = () => {
-    if (selectedNote) {
-      let titleTextOnly = extractContent(selectedNote.title, true)
-      return titleTextOnly;
-    } else {
-      return "none"
-    }
-  }
-
-  const { download } = specific.useTextDownloader();
-
   
-  const handleDownload = () => {
-    let bodyArr = [];
-    bodyArr.push(txtBody())
-    let bodyStr = bodyArr.toString()
-    let titleArr = [];
-    titleArr.push(txtTitle())
-    let titleStr = titleArr.toString()
-    download({ 
-      data: bodyStr,
-      name: `${titleStr}`  
-    });
-  }
 
   return (
       <div>
@@ -106,16 +59,9 @@ const Header = () => {
                 </div>
               </div>
               <div className='self-center'>
-                {selectedNote ? 
-                <button onClick={handleDownload} className="flex flex-row items-center" data-tooltip-target="save-tooltip" type="button">
-                  <svg className="w-7 h-7 text-slate-600 dark:text-sky-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>
-                </button> : 
-                <button className="flex flex-row items-center cursor-not-allowed" data-tooltip-target="save-tooltip" type="button">
-                  <svg className="w-7 h-7 text-slate-400 dark:text-sky-100" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>
-                </button>
-                }
+                <DownloadBtn />
                 <div id="save-tooltip" role="tooltip" className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                  Save
+                  Download
                 <div className='tooltip-arrow' data-popper-arrow></div>
                 </div>
               </div>
